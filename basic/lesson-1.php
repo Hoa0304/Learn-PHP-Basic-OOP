c74545d4310337cd2f51a5d646c702a9d2783f5c
Hoa0304 <tranthicamhoa2k4@gmail.com>
2023-10-16 08:00:11 +0700

    operators

--- basic/lesson-1.php
+++ basic/lesson-1.php
@@ -1,5 +1,25 @@
 <?php
 $x = 3;
-$y = 4;
+$y = '4';
+$z = 'b';
 var_dump($x == $y);
+// bool(false)
+var_dump($x !== $z);
+//bool(true)
+var_dump($x != $y);
+//bool(true)
+var_dump($x === $y);
+// bool(false)
+var_dump($x !== $y);
+//bool(true)
+
+$a = ($x > 5)? $x :0 ;
+var_dump($a);
+//kqua : 0
+
+$b = 'Hoa cute vô đối';
+$b = $b ?? 'Hoa la ai?';
+var_dump($b);
+//string(19) "Hoa cute vô đối"
+
 ?>
